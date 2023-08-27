ec5fed3c7650d6c8b5d56ea75fbf1359efcff79f
TanviSayare <130274080+TanviSayare@users.noreply.github.com>
2023-08-27 21:00:43 +0400

    Add files via upload

---
+++ Throws.java
@@ -0,0 +1,20 @@
+public class Throws
+{
+ public static int div(int x, int y) throws Exception
+ {
+  return(x/y);
+ }
+ public static void main(String args[])
+ {
+  try
+  {
+   int d;
+   d = div(10,0);
+   System.out.println("Division is " +d);
+  }
+  catch(Exception ex)
+  {
+   ex.printStackTrace();
+  }
+ }
+}
