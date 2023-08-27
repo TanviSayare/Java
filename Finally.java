8024ab5cc0e07c4a1e21b96b0907b0a9677182ac
TanviSayare <130274080+TanviSayare@users.noreply.github.com>
2023-08-27 20:38:18 +0400

    Add files via upload

---
+++ Finally.java
@@ -0,0 +1,25 @@
+public class Finally
+{
+ public static void main(String args[])
+ {
+  try
+  {
+   int a[] = {10,20,30,40,50};
+   System.out.println(a[0]);
+   System.out.println(a[1]);
+   System.out.println(a[2]);
+   System.out.println(a[3]);
+   System.out.println(a[4]);
+   System.out.println(a[5]);
+   System.out.println("Program Terminated");
+  }
+  catch(Exception ex)
+  {
+   ex.printStackTrace();
+  }
+  finally
+  {
+   System.out.println("Program Terminated");
+  }
+ }
+}
